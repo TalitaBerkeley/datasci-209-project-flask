d3f3f49593c6ccccb41e670705c74d72ca84caae
TALITA <talitamesquita@berkeley.edubash>
2026-02-25 13:32:02 -0800

    Add D3 map example

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template
+import pandas as pd
+import os
+
 app = Flask(__name__)
+APP_FOLDER = os.path.dirname(os.path.realpath(__file__))
 
-@app.route('/')
-def w209():
-    file='about9.jpg'
-    return render_template('w209.html',file=file)
+@app.route("/")
+def map_page():
+    return render_template("index.html")
 
-if __name__ == '__main__':
+@app.route("/getData/<int:year>")
+def getData(year):
+    revenue = pd.read_csv(os.path.join(APP_FOLDER, "static/data/1_Revenues.csv"))
+
+    if year < 1942 or year > 2008:
+        return "Error in the year range", 400
+
+    filteredRevenue = revenue[revenue["Year4"] == year][
+        ["Name", "Year4", "Total Revenue", "Population (000)"]
+    ]
+
+    return filteredRevenue.to_json(orient="records")
+
+if __name__ == "__main__":
     app.run()
+
